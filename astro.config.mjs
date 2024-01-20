5e674c9ab9e2dde12b6795597137d75dba031557
ivanlalic <ivanlalic@gmail.com>
2024-01-20 01:47:30 -0300

    AstroNetlifyAdapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 // import svelte from '@astrojs/svelte'
 
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lexingtonthemes.com',
   integrations: [tailwind(), sitemap()],
   output: 'hybrid',
+  adapter: netlify()
 });
